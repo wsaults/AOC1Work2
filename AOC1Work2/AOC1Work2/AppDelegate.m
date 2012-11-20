3cd4fd99cc8fedc5af71190cb6aec0b3f927dca0
William Saults <wsaults@email.itt-tech.edu>
2012-11-19 23:46:30 -0600

    Comment adjustment.

--- AOC1Work2/AOC1Work2/AppDelegate.m
+++ AOC1Work2/AOC1Work2/AppDelegate.m
@@ -2,8 +2,8 @@
 //  AppDelegate.m
 //  AOC1Work2
 //
-//  Created by Will Saults on 11/19/12.
-//  Copyright (c) 2012 UTVCA. All rights reserved.
+//  Created by Will Saults on 11/19/12. AOC1_1212
+//  Copyright (c) 2012 Fullsail. All rights reserved.
 //
 
 #import "AppDelegate.h"
