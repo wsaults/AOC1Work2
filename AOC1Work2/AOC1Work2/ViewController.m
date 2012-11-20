b51dd3972f195f39839565ea540b8a92b9b9e84b
William Saults <wsaults@email.itt-tech.edu>
2012-11-20 00:07:15 -0600

    Definitions

--- AOC1Work2/AOC1Work2/ViewController.m
+++ AOC1Work2/AOC1Work2/ViewController.m
@@ -7,14 +7,7 @@
 //
 
 #import "ViewController.h"
-#define kLabelMargin 5
-#define kLabelHeaderWidth 75
-#define kLabelTitleHeight 30
-#define kLabelHeight 20
-#define kLabelHeaderFontSize [UIFont boldSystemFontOfSize:14]
-#define kLabelFontSize [UIFont systemFontOfSize:14]
-#define kLabelHeaderTextColor [UIColor colorWithRed:0.361 green:0.475 blue:0.169 alpha:1] /*#5c792b*/
-#define kLabelTextColor [UIColor colorWithRed:1 green:0.31 blue:0.141 alpha:1] /*#ff4f24*/
+#import "Definitions.h"
 
 @interface ViewController ()
 
